fb9d5a7eaf4ae0894e93d3055fa0881900331425
andre.cintra <andre.cintra@zup.com.br>
2023-06-05 14:13:17 -0300

    fix: adjusting for review

--- stackspot-actions/bitbucket/create-repo-bitbucket/script.py
+++ stackspot-actions/bitbucket/create-repo-bitbucket/script.py
@@ -1,180 +1,203 @@
 import json
+from typing import Any
 import requests
 
 
-class Exceptions(Exception):
+class BitbucketCreateRepoException(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
 
-    @classmethod
-    def workspace_not_found(cls, workspace):
-        return cls(f"The {workspace} workspace was not found.")
 
-    @classmethod
-    def project_name_to_short(cls, project_name):
-        return cls(
+class WorkspaceNotFound(BitbucketCreateRepoException):
+    def __init__(self, workspace):
+        super().__init__(f"The {workspace} workspace was not found.")
+
+
+class WorkspaceExistsGeneralError(BitbucketCreateRepoException):
+    def __init__(self, err):
+        super().__init__(
+            f"An error ocurred while checking if the workspace exists: {err}"
+        )
+
+
+class ProjectNameTooShort(BitbucketCreateRepoException):
+    def __init__(self, project_name):
+        super().__init__(
             f"The name {project_name} is too short. It must contain at least 3 characters."
         )
 
-    @classmethod
-    def project_creation_error(cls, err):
-        return cls(f"An error ocurred while creating the project: {err}")
 
-    @classmethod
-    def repository_exists_general_error(cls, err):
-        return cls(f"An error ocurred while checking if the repository exists: {err}")
-
-    @classmethod
-    def workspace_exists_general_error(cls, err):
-        return cls(f"An error ocurred while checking if the workspace exists: {err}")
-
-    @classmethod
-    def create_repository_error(cls, err):
-        return cls(f"An error ocurred while creating the repository: {err}")
+class ProjectCreationError(BitbucketCreateRepoException):
+    def __init__(self, err):
+        super().__init__(f"An error ocurred while creating the project: {err}")
 
 
-def __workspace_exists(workspace_name: str):
-    """
-    Checks if the workspace exists
-    Workspaces can not be created by API integration
-    """
-    print(
-        f"> Checking if the '{workspace_name}' workspace exists in your organization."
-    )
-
-    url = f"{base_url}/workspaces/{workspace_name}/permissions"
-
-    try:
-        response = requests.get(url, headers=get_headers)
-        response.raise_for_status()
-        print(f"> Workspace '{workspace_name}' found.")
-    except requests.exceptions.HTTPError as e:
-        if e.response.status_code in (
-            requests.codes.not_found,
-            requests.codes.forbidden,
-            requests.codes.unauthorized,
-        ):
-            raise Exceptions.workspace_not_found(workspace_name)
-        raise Exceptions.workspace_exists_general_error(e)
-
-
-def __repository_exists(workspace_name: str, repo_name: str):
-    """
-    Checks if the repository exists
-    """
-    print(
-        f"> Checking if the '{repo_name}' repository exists in the workspace {workspace_name}."
-    )
-
-    url = f"{base_url}/repositories/{workspace_name}/{repo_name}"
-
-    try:
-        response = requests.get(url, headers=get_headers)
-        response.raise_for_status()
-        print(f">'{repo_name}' already exists.")
-        return True
-    except requests.exceptions.HTTPError as e:
-        if e.response.status_code == requests.codes.not_found:
-            print(f">'{repo_name}' not found. It will be created.")
-            return False
-        raise Exceptions.repository_exists_general_error(e)
-
-
-def __project_exists(workspace_name: str, project_name: str):
-    """
-    Checks if the informed project exists
-    """
-    print(
-        f"> Checking if the '{project_name}' project exists in the workspace {workspace_name}."
-    )
-    url = f"{base_url}/workspaces/{workspace_name}/projects"
-
-    try:
-        response = requests.get(url, get_headers)
-        response.raise_for_status()
-        data = response.json()
-        values = data.get("values", [])
-        project_exists = [
-            val
-            for val in values
-            if val.get("key") == project_name or val.get("name") == project_name
-        ]
-        if project_exists:
-            print(
-                f"> Project '{project_name}' project found in the workspace {workspace_name}."
-            )
-            project = project_exists.pop()
-            return project.get("key")
-    except requests.exceptions.HTTPError as e:
-        print(
-            f"> Project '{project_name}' not found in the workspace {workspace_name}."
+class RepositoryExistsGeneralError(BitbucketCreateRepoException):
+    def __init__(self, err):
+        super().__init__(
+            f"An error ocurred while checking if the repository exists: {err}"
         )
-        if e.response.status_code == requests.codes.not_found:
-            return None
 
 
-def __create_project(workspace_name: str, project_name: str):
-    """
-    Creates a new Bitbucket Project in the workspace
-    """
-    print(f"> Creating project '{project_name}' in the workspace {workspace_name}.")
-    url = f"{base_url}/workspaces/{workspace_name}/projects"
-
-    if len(project_name) < 3:
-        raise Exceptions.project_name_to_short(project_name)
-
-    key = project_name.replace(" ", "").replace("_", "").replace("-", "")[:3].upper()
-    print(f"> Project key set to {key}")
-
-    payload = json.dumps({"name": project_name, "key": key})
-
-    try:
-        response = requests.post(url, headers=post_headers, data=payload)
-        response.raise_for_status()
-        print(f"> Project '{project_name}' created in the workspace {workspace_name}.")
-    except requests.exceptions.HTTPError as e:
-        raise Exceptions.project_creation_error(e)
-
-    return key
+class CreateRepositoryError(BitbucketCreateRepoException):
+    def __init__(self, err):
+        super().__init__(f"An error ocurred while creating the repository: {err}")
 
 
-def __create_repository(workspace_name: str, project_key: str, repo_name: str):
-    """
-    Creates a new repository on bitbucket
-    """
-    print(f"> Creating repository '{repo_name}' in the project {project_key}.")
+class Runner:
+    def __init__(self, metadata) -> None:
+        self.base_url = "https://api.bitbucket.org/2.0"
+        inputs = metadata.inputs
+        token = inputs.get("token")
+        self.get_headers = {
+            "Authorization": f"Bearer {token}",
+            "Accept": "application/json",
+        }
+        self.post_headers = {"Content-Type": "application/json"}
+        self.post_headers.update(self.get_headers)
 
-    url = f"{base_url}/repositories/{workspace_name}/{repo_name}"
+        self.workspace_name = inputs.get("org")
+        self.project_name = inputs.get("project_name")
+        self.repo_name = inputs.get("repo_name")
 
-    payload = json.dumps({"scm": "git", "project": {"key": project_key}})
+    def __call__(self) -> Any:
+        self.__workspace_exists()
+        if not self.__repository_exists():
+            project_key = self.__project_exists()
+            if not project_key:
+                project_key = self.__create_project()
+            self.__create_repository(project_key)
 
-    try:
-        response = requests.post(url, data=payload, headers=post_headers)
-        response.raise_for_status()
-        print(f"Repository {repo_name} successfully created.")
-    except requests.exceptions.HTTPError as e:
-        raise Exceptions.create_repository_error(e)
+    def __workspace_exists(self):
+        """
+        Checks if the workspace exists
+        Workspaces can not be created by API integration
+        """
+        print(
+            f"> Checking if the '{self.workspace_name}' workspace exists in your organization."
+        )
+
+        url = f"{self.base_url}/workspaces/{self.workspace_name}/permissions"
+
+        try:
+            response = requests.get(url, headers=self.get_headers)
+            response.raise_for_status()
+            print(f"> Workspace '{self.workspace_name}' found.")
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code in (
+                requests.codes.not_found,
+                requests.codes.forbidden,
+                requests.codes.unauthorized,
+            ):
+                raise WorkspaceNotFound(self.workspace_name)
+            raise WorkspaceExistsGeneralError(e)
+
+    def __repository_exists(self):
+        """
+        Checks if the repository exists
+        """
+        print(
+            f"> Checking if the '{self.repo_name}' repository exists in the workspace {self.workspace_name}."
+        )
+
+        url = f"{self.base_url}/repositories/{self.workspace_name}/{self.repo_name}"
+
+        try:
+            response = requests.get(url, headers=self.get_headers)
+            response.raise_for_status()
+            print(f">'{self.repo_name}' already exists.")
+            return True
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code == requests.codes.not_found:
+                print(f">'{self.repo_name}' not found. It will be created.")
+                return False
+            raise RepositoryExistsGeneralError(e)
+
+    def __project_exists(self):
+        """
+        Checks if the informed project exists
+        """
+        print(
+            f"> Checking if the '{self.project_name}' project exists in the workspace {self.workspace_name}."
+        )
+        url = f"{self.base_url}/workspaces/{self.workspace_name}/projects"
+
+        try:
+            response = requests.get(url, self.get_headers)
+            response.raise_for_status()
+            data = response.json()
+            values = data.get("values", [])
+            project_exists = [
+                val
+                for val in values
+                if val.get("key") == self.project_name
+                or val.get("name") == self.project_name
+            ]
+            if project_exists:
+                print(
+                    f"> Project '{self.project_name}' project found in the workspace {self.workspace_name}."
+                )
+                project = project_exists.pop()
+                return project.get("key")
+        except requests.exceptions.HTTPError as e:
+            print(
+                f"> Project '{self.project_name}' not found in the workspace {self.workspace_name}."
+            )
+            if e.response.status_code == requests.codes.not_found:
+                return None
+
+    def __create_project(self):
+        """
+        Creates a new Bitbucket Project in the workspace
+        """
+        print(
+            f"> Creating project '{self.project_name}' in the workspace {self.workspace_name}."
+        )
+        url = f"{self.base_url}/workspaces/{self.workspace_name}/projects"
+
+        if len(self.project_name) < 3:
+            raise ProjectNameTooShort(self.project_name)
+
+        key = (
+            self.project_name.replace(" ", "")
+            .replace("_", "")
+            .replace("-", "")[:3]
+            .upper()
+        )
+        print(f"> Project key set to {key}")
+
+        payload = json.dumps({"name": self.project_name, "key": key})
+
+        try:
+            response = requests.post(url, headers=self.post_headers, data=payload)
+            response.raise_for_status()
+            print(
+                f"> Project '{self.project_name}' created in the workspace {self.workspace_name}."
+            )
+        except requests.exceptions.HTTPError as e:
+            raise ProjectCreationError(e)
+
+        return key
+
+    def __create_repository(self, project_key: str):
+        """
+        Creates a new repository on bitbucket
+        """
+        print(f"> Creating repository '{self.repo_name}' in the project {project_key}.")
+
+        url = f"{self.base_url}/repositories/{self.workspace_name}/{self.repo_name}"
+
+        payload = json.dumps({"scm": "git", "project": {"key": project_key}})
+
+        try:
+            response = requests.post(url, data=payload, headers=self.post_headers)
+            response.raise_for_status()
+            print(f"Repository {self.repo_name} successfully created.")
+        except requests.exceptions.HTTPError as e:
+            raise CreateRepositoryError(e)
 
 
 def run(metadata):
-    global base_url
-    global get_headers
-    global post_headers
-    base_url = "https://api.bitbucket.org/2.0"
-    inputs = metadata.inputs
-    token = inputs.get("token")
-    get_headers = {"Authorization": f"Bearer {token}", "Accept": "application/json"}
-    post_headers = {"Content-Type": "application/json"}
-    post_headers.update(get_headers)
-
-    workspace_name = inputs.get("workspace_name")
-    project_name = inputs.get("project_name")
-    repo_name = inputs.get("repo_name")
-
-    __workspace_exists(workspace_name)
-    if not __repository_exists(workspace_name, repo_name):
-        project_key = __project_exists(workspace_name, project_name)
-        if not project_key:
-            project_key = __create_project(workspace_name, project_name)
-        __create_repository(workspace_name, project_key, repo_name)
+    runner = Runner(metadata)
+    runner()
