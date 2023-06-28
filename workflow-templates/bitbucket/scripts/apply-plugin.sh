ec91df28f6b82086e6bf9831edf86deed6d24883
andre.cintra <andre.cintra@zup.com.br>
2023-06-28 16:33:51 -0300

    fix apply plugin template

--- workflow-templates/bitbucket/scripts/apply-plugin.sh
+++ workflow-templates/bitbucket/scripts/apply-plugin.sh
@@ -13,24 +13,6 @@ _print_message_if_debug_enabled(){
 _print_message_if_debug_enabled "$green➤ DEBUG$two_dots_unicode Plugin name$two_dots_unicode $u_cyan$plugin_name";
 
 args=();
-connectors=()
-if [ -z "$plugin_connection_interfaces" ] || [ "$plugin_connection_interfaces" == "null" ]; then
-    _print_message_if_debug_enabled "$green➤ DEBUG$two_dots_unicode no connection-interfaces found";
-else
-    _print_message_if_debug_enabled "$green➤ DEBUG$two_dots_unicode plugin_connection_interfaces $plugin_connection_interfaces";
-
-    for json_object in $(echo "$plugin_connection_interfaces" | jq -c '.[]'); do
-        name=$(echo "$json_object" | jq -r '.name')
-        type=$(echo "$json_object" | jq -r '.type')
-        conn="--connectors.$type $name"
-        connectors+=("$conn")
-    done
-
-    _print_message_if_debug_enabled "$green➤ DEBUG$two_dots_unicode Concatened Conns $concatened_conn";
-fi;
-
-concatened_conn=$(IFS=' ' && echo "${connectors[*]}")
-
 if [ -z "$plugin_inputs" ] || [ "$plugin_inputs" == "null" ]; then
     _print_message_if_debug_enabled "$green➤ DEBUG$two_dots_unicode no inputs-json found.";
 else
@@ -39,10 +21,19 @@ else
     _print_message_if_debug_enabled "$green➤ DEBUG$two_dots_unicode '--inputs-json $plugin_inputs_print'";
 fi;
 
+connectors=();
+if [ -z "$plugin_connection_interfaces" ] || [ "$plugin_connection_interfaces" == "null" ]; then
+    _print_message_if_debug_enabled "$green➤ DEBUG$two_dots_unicode no connection-interfaces found";
+else
+    _print_message_if_debug_enabled "$green➤ DEBUG$two_dots_unicode plugin_connection_interfaces $plugin_connection_interfaces";
+    connectors=$(echo "$plugin_connection_interfaces" | jq -rC '. | join(" ")')
+    _print_message_if_debug_enabled "$green➤ DEBUG$two_dots_unicode Concatened Conns $connectors";
+fi;
+
 echo -e "$green⚡ Applying Plugin $u_cyan$plugin_name\n";
 
-echo -e $yellow $workflow_config apply plugin $plugin_name --skip-warning $(echo "${args[@]}") $concatened_conn "\n";
-$workflow_config apply plugin $plugin_name --skip-warning "${args[@]}" $concatened_conn;
+echo -e $yellow $workflow_config apply plugin $plugin_name --skip-warning $(echo "${args[@]}") $connectors "\n";
+$workflow_config apply plugin $plugin_name --skip-warning "${args[@]}" $connectors;
 exit_code_of_command=$?;
 if [ "$exit_code_of_command" != "0" ]; then
     echo -e "\Apply plugin failed$two_dots_unicode $plugin_name";
